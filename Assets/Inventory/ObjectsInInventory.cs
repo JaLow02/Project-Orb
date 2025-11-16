6574ade01ffbff7d0906a5984741440ce21660b3
JaLow02 <jalow02@gmail.com>
2025-11-16 19:16:41 +0100

    Add Player Jump
    added player jump
    + added triggers and rigidbodies to items in inventory scene

---
+++ Assets/Inventory/ObjectsInInventory.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class ObjectsInInventory
+{
+    public static bool bowEquiped = false;
+    public static bool swordEquiped = false;
+    public static int speedBoost = 0;
+    public static int jumpBoost = 0;
+}
