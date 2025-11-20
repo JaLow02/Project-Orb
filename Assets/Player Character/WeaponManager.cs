ae1011513b50bfed8a7be60e85083d970633fbf3
borabirr <84836865+borabirr@users.noreply.github.com>
2025-11-20 23:07:39 +0100

    Garlic fix

--- Assets/Player Character/WeaponManager.cs
+++ Assets/Player Character/WeaponManager.cs
@@ -12,7 +12,7 @@ public class WeaponManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (garlicActivated >= ObjectsInInventory.garlicAmount)
+        if (garlicActivated <= ObjectsInInventory.garlicAmount)
         {
             SpawnWeapon();
             ++ garlicActivated;
